dee2dbfaa3e836761a8d3eccb282c668fddf991d
Steven <level2fast@gmail.com>
2023-04-17 20:29:37 -0700

    cleanup

--- Barbershop_Prototype/NGS_Studio/NGS_Studio/Models/User.cs
+++ Barbershop_Prototype/NGS_Studio/NGS_Studio/Models/User.cs
@@ -31,52 +31,5 @@ namespace NGS_Studio.Models
 
 		public string ImageUrl { get; set; }
 
-		//public static FirebaseClient firebase = new FirebaseClient("https://ngs-studios-bd357-default-rtdb.firebaseio.com/");
-		//public static async Task<List<User>> GetAllUsers()
-		//{
-
-		//	return (await firebase
-		//	  .Child("User")
-		//	  .OnceAsync<User>()).Select(item => new User
-		//	  {
-		//		  PhoneNumber = item.Object.PhoneNumber,
-		//		  Name = item.Object.Name
-		//	  }).ToList();
-		//}
-		//public static async Task AddUser(string phonenumber, string name)
-		//{
-
-		//	await firebase
-		//	  .Child("Users")
-		//	  .PostAsync(new User() { PhoneNumber = phonenumber, Name = name });
-		//}
-		//public static async Task<User> GetUser(string PhoneNumber)
-		//{
-		//	var allUsers = await GetAllUsers();
-		//	await firebase
-		//	  .Child("Users")
-		//	  .OnceAsync<User>();
-		//	return allUsers.Where(a => a.PhoneNumber == PhoneNumber).FirstOrDefault();
-		//}
-		//public static async Task UpdateUser(string PhoneNumber, string name)
-		//{
-		//	var toUpdateUser = (await firebase
-		//	  .Child("Users")
-		//	  .OnceAsync<User>()).Where(a => a.Object.PhoneNumber == PhoneNumber).FirstOrDefault();
-
-		//	await firebase
-		//	  .Child("Users")
-		//	  .Child(toUpdateUser.Key)
-		//	  .PutAsync(new User() { PhoneNumber = PhoneNumber, Name = name });
-		//}
-
-		//public static async Task DeleteUser(string PhoneNumber)
-		//{
-		//	var toDeleteUser = (await firebase
-		//	  .Child("Users")
-		//	  .OnceAsync<User>()).Where(a => a.Object.PhoneNumber == PhoneNumber).FirstOrDefault();
-		//	await firebase.Child("Users").Child(toDeleteUser.Key).DeleteAsync();
-
-		//}
 	}
 }
